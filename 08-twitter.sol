59e7dd8771666ac040671235691162fed9b7d7fd
farawaystar <md.sh1@protonmail.com>
2025-03-17 15:04:53 +0100

    added 08

--- 08-twitter.sol
+++ 08-twitter.sol
@@ -10,6 +10,7 @@ pragma solidity ^0.8.0;
 
 contract Twitter {
 mapping(address => Tweet[] ) public tweets;
+uint16 constant MAX_TWEET_LENGTH = 280;
 
 struct Tweet {
 	address author;
@@ -19,6 +20,7 @@ struct Tweet {
 }
 
 function createTweet(string memory _tweet) public {
+    require(bytes(_tweet).length <= MAX_TWEET_LENGTH, "tweet length exceeded");
 	Tweet memory newTweet = Tweet({
 		author: msg.sender,
 		timestamp: block.timestamp,
@@ -29,11 +31,11 @@ function createTweet(string memory _tweet) public {
 	tweets[msg.sender].push(newTweet);
 }
 
-function getTweet(address _owner, uint _i) public view returns (Tweet memory) {
-	return tweets [_owner][_i];
+function getTweet(uint _i) public view returns (Tweet memory) {
+	return tweets [msg.sender][_i];
 	}
 
-function getAllTweets(address _owner) public view returns (Tweet[] memory){
-	return tweets[_owner];
+function getAllTweets() public view returns (Tweet[] memory){
+	return tweets[msg.sender];
 	}
 }
