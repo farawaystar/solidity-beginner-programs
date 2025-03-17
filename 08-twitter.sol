4446ec3d40bdabef6f99e9213cb98d1d2b4b501e
farawaystar <md.sh1@protonmail.com>
2025-03-17 13:57:35 +0100

    added 08

--- 08-twitter.sol
+++ 08-twitter.sol
@@ -9,13 +9,17 @@ pragma solidity ^0.8.0;
 // 5️⃣ Add array of tweets 
 
 contract Twitter {
-mapping(address => string ) public tweets;
+mapping(address => string[] ) public tweets;
 
 function createTweet(string memory _tweet) public {
-	tweets[msg.sender] = _tweet;
+	tweets[msg.sender].push(_tweet);
 	}
 
-function getTweet(address _owner) public view returns (string memory) {
-	return tweets [_owner];
+function getTweet(address _owner, uint _i) public view returns (string memory) {
+	return tweets [_owner][_i];
+	}
+
+function getAllTweets(address _owner) public view returns (string[] memory){
+	return tweets[_owner];
 	}
 }
