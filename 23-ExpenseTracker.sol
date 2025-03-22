0747039a1fbf55e333217b98233298df9fc3cee8
farawaystar <md.sh1@protonmail.com>
2025-03-23 00:25:15 +0100

    added 24-ExpenseTracker.sol

---
+++ 23-ExpenseTracker.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+
+// 1️⃣ Create a loop to calcualte all expenses for the user
+// HINT: Create a total expenses variable with uint type
+// HINT: Loop over expenses array with for loop
+// HINT: add up all expenses cost
+// HINT: return total expenses
+
+pragma solidity ^0.8.0;
+
+contract ExpenseTracker{
+
+
+// mapping(address => Expense[]) public Expenses;
+
+Expense[] public Expenses;
+
+struct Expense {
+    address user;
+    string description;
+    uint256 amount;
+}
+
+
+constructor() {
+    Expenses.push(Expense(msg.sender, "something 1", 10));
+    Expenses.push(Expense(msg.sender, "something 2", 5));
+    Expenses.push(Expense(msg.sender, "something 3", 7));
+}
+
+
+function addExpense(string memory _description, uint256 _amount) public{
+	Expenses.push(Expense(msg.sender, _description, _amount));
+	}
+
+function getTotalExpenses(address _user) public view returns (uint256) {
+	uint256 totalExpenses;
+	for (uint i = 0; i < Expenses.length; i++) {
+		if (Expenses[i].user == _user){
+			totalExpenses += Expenses[i].amount;
+		}	
+	}
+	return totalExpenses;
+	}
+
+}
+
